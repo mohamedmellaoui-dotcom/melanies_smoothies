2fafe6c07769b42160a44495d29cb8d31ceb09e6
mohamedmellaoui-dotcom <mohamed.mellaoui@smile.fr>
2026-05-26 20:54:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ if ingredients_list:
 
         st.subheader(f"{fruit_chosen} Nutrition Informations")
 
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on]")
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # INSERT
